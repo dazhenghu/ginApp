0e919e9e135fce9e1164c209d42e745e351194f8
huhao <huhao@qianshengqian.com>
2018-05-30 01:01:26 +0800

    增加静态资源配置

--- config/app.go
+++ config/app.go
@@ -1,8 +1,19 @@
 package config
 
+/**
+app配置
+ */
 type AppConfig struct {
     Addr        string             `yaml:"addr"`          // 监听端口
-    Pulic       string             `yaml:"public"`        // 静态资源路径
+    Pulic       StaticConfig       `yaml:"public"`        // 静态资源路径
     ViewBaseDir string             `yaml:"viewBaseDir"`   // view路径
     Dblist      map[string]DbConfg `yaml:"dblist"`        // 数据库连接列表
+}
+
+/**
+静态资源配置
+ */
+type StaticConfig struct {
+    RelativePath string `yaml:"relativePath"`
+    Root         string `yaml:"root"`
 }
