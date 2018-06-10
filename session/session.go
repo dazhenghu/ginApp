fe6a1d9780c19c5792358117bdba4a34f6202bd6
huhao <huhao@qianshengqian.com>
2018-06-10 23:12:02 +0800

    默认sessionkey

--- session/session.go
+++ session/session.go
@@ -15,7 +15,7 @@ const (
     SESSION_TYPE_MONGO     string = "mongo"     // 保存在mongo中
 )
 
-var DefaultKey string = "" // cookie中保存session的key
+var DefaultKey string = "ginapp-session" // cookie中保存session的key
 
 func NewStore(appConfig *config.AppConfig) (store sessions.Store, err error) {
     sessionConf := appConfig.SessionCnf.ConnectCnf
