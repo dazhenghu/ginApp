c5b8b51c155cc64604fa210597cf2ef454a1f820
huhao <huhao@qianshengqian.com>
2018-06-14 01:42:39 +0800

    session实例创建好后返回

--- session/session.go
+++ session/session.go
@@ -23,12 +23,14 @@ func NewStore(appConfig *config.AppConfig) (store sessions.Store, err error) {
     switch appConfig.SessionCnf.Type {
     case SESSION_TYPE_COOKIE:
         store = sessions.NewCookieStore()
+        return
     case SESSION_TYPE_REDIS:
         size, _ := strconv.Atoi(sessionConf["size"])
         network := sessionConf["network"]
         adress := sessionConf["adress"]
         password := sessionConf["password"]
         store, err = sessions.NewRedisStore(size, network, adress, password)
+        return
     }
 
     err = errors.New(fmt.Sprintf("not valid session type:%s", appConfig.SessionCnf.Type))
