df00f5048da76807b3552a19cf763bd8b511971c
huhao <huhao@qianshengqian.com>
2018-07-10 08:40:20 +0800

    format格式化输出

--- logs/logger.go
+++ logs/logger.go
@@ -33,42 +33,34 @@ func Fatal(args ...interface{}) {
     log.Fatal(args...)
 }
 
-// Debugf logs a message at level Debug on the standard logger.
 func Debugf(format string, args ...interface{}) {
     log.Debugf(format, args...)
 }
 
-// Printf logs a message at level Info on the standard logger.
 func Printf(format string, args ...interface{}) {
     log.Printf(format, args...)
 }
 
-// Infof logs a message at level Info on the standard logger.
 func Infof(format string, args ...interface{}) {
     log.Infof(format, args...)
 }
 
-// Warnf logs a message at level Warn on the standard logger.
 func Warnf(format string, args ...interface{}) {
     log.Warnf(format, args...)
 }
 
-// Warningf logs a message at level Warn on the standard logger.
 func Warningf(format string, args ...interface{}) {
     log.Warningf(format, args...)
 }
 
-// Errorf logs a message at level Error on the standard logger.
 func Errorf(format string, args ...interface{}) {
     log.Errorf(format, args...)
 }
 
-// Panicf logs a message at level Panic on the standard logger.
 func Panicf(format string, args ...interface{}) {
     log.Panicf(format, args...)
 }
 
-// Fatalf logs a message at level Fatal on the standard logger.
 func Fatalf(format string, args ...interface{}) {
     log.Fatalf(format, args...)
 }
@@ -77,7 +69,6 @@ func WithFields(fields log.Fields) *log.Entry {
     return log.WithFields(fields)
 }
 
-// SetOutput sets the standard logger output.
 func SetOutput(out io.Writer) {
     log.SetOutput(out)
 }
